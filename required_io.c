d291f15fff689386881847891c637b252366f020
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2018-04-05 17:11:21 -0500

    Just playing around github a bit/added some comments to the code

--- required_io.c
+++ required_io.c
@@ -2,27 +2,28 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 100
+#define MAX 100   //Maybe a greater/lesser value defined here    
 
 int main(){
 	
 	int x;
 	int y;
 	char g[MAX]; //instead of the user initializing the string here they have the
-		     //option to initalize it else where. An arbitrary MAX size is 
-		     //defined here. (Let me know what everyone thinks about this)
+		         //option to initalize it else where. An arbitrary MAX size is 
+		         //defined here. (Let me know what everyone thinks about this)
 
 	y = 11;
 	x = 3 * y;
 	printf("%d\n", x); 
 	
-	strncpy(g ,"hello world", sizeof(g)); //this line would be omitted of course if 
+	strncpy(g ,"hello world", sizeof(g));
+	                      //this line would be omitted of course if 
 					      //the user decides to initallize the
 					      //string in the header
-	printf("%s\n", g);
+	printf("%s\n", g);    
 
-	for(int i = 0; i < strlen(g); i++){
-		printf("%c\n", g[i]);
+	for(int i = 0; i < strlen(g); i++){  //prints the character array
+		printf("%c\n", g[i]);            //vertically
 	}
 
 }
