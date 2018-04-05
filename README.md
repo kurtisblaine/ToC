83d47285aba18bbc61558a4ce2443f7c752e3b89
Landon Frederes <landon@frederes.com>
2018-04-04 17:47:58 -0700

    Removed my test line from the README. Added c file from slack containing our required output from specified input.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # c_scanner_parser
 CSC466 ToC
-
-Test line added by Landon F.
---
+++ required_io.c
@@ -0,0 +1,49 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~target language~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int main(){
+	
+	int x;
+	int y;
+	char g[MAX]; //instead of the user initializing the string here they have the
+		     //option to initalize it else where. An arbitrary MAX size is 
+		     //defined here. (Let me know what everyone thinks about this)
+
+	y = 11;
+	x = 3 * y;
+	printf("%d\n", x); 
+	
+	strncpy(g ,"hello world", sizeof(g)); //this line would be omitted of course if 
+					      //the user decides to initallize the
+					      //string in the header
+	printf("%s\n", g);
+
+	for(int i = 0; i < strlen(g); i++){
+		printf("%c\n", g[i]);
+	}
+
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~target language~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~input format~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Input format:
+
+	   vari x
+	   vari y
+	   vars g
+	
+	   y = 11
+	   x = 3 * y
+	   printi x
+
+	   g = "hello world"
+	   printsh g
+	   printsv g
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~input format~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
